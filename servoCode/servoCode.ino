a180f987b8aa2b6228dc4079775f41667d9b4733
Manuel Palmeira <mnp.manuel@gmail.com>
2015-07-13 15:53:26 +0100

    optimização de performance

--- servoCode/servoCode.ino
+++ servoCode/servoCode.ino
@@ -8,7 +8,7 @@
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
  
-int pos = 0;    // variable to store the servo position 
+int pos = 90;    // variable to store the servo position 
  
 void setup() 
 { 
@@ -18,12 +18,12 @@ void setup()
  
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  for(pos = 90; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(5);                       // waits 15ms for the servo to reach the position 
   } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  for(pos = 90; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(5);                       // waits 15ms for the servo to reach the position 
